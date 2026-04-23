d89b7342be7c19b3cd2c9a3b0d5ef5055a5845fc
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-04-23 15:35:04 +0530

    Update hooks.py

--- humain_learning/hooks.py
+++ humain_learning/hooks.py
@@ -268,3 +268,4 @@ fixtures = [
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+after_install = "humain_learning.humain_learning.setup.after_install"
