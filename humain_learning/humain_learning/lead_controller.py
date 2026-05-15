a5c01ec11d8af513b64361b262c8e7abefe0c059
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-05-15 16:36:55 +0530

    update

--- humain_learning/humain_learning/lead_controller.py
+++ humain_learning/humain_learning/lead_controller.py
@@ -1,16 +1,25 @@
 import frappe
 
 def assign_campaign(lead,_):
-    if lead.source != "Facebook":
-        return
-    
-    campaign = frappe.db.get_value("Campaign Purpose", {"form_id": lead.facebook_form_id},["parent","action"], as_dict=True)
-    
-    if not campaign:
-        return
-    
-    lead.custom_campaign = campaign.parent
-    lead.custom_actionable = campaign.action
+    if lead.facebook_form_id:
+        campaign = frappe.db.get_value("Campaign Purpose", {"form_id": lead.facebook_form_id},["parent","action"], as_dict=True)
+        if campaign:
+            lead.custom_campaign = campaign.parent
+            lead.custom_actionable = campaign.action
+        else: 
+            return
+    elif lead.custom_utm_campaign:
+        campaign_name = lead.custom_utm_campaign.strip()
+
+        if frappe.db.exists("Marketing Campaign", campaign_name):
+            lead.custom_campaign = campaign_name
+        else:
+            frappe.get_doc({
+                "doctype": "Marketing Campaign",
+                "name": campaign_name,
+                "campaign_description": ""
+            }).insert(ignore_permissions=True, ignore_if_duplicate=True)
+            lead.custom_campaign = campaign_name
     
 
 def standardize_mobile(doc, _):
@@ -27,3 +36,4 @@ def standardize_mobile(doc, _):
         doc.mobile_no = default_country_code + digits[1:]
     else:
         return
+    
