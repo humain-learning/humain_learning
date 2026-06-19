80c37db5da84de960b73c62d90edf108dc1fbf98
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-06-19 15:51:10 +0530

    update

--- humain_learning/zoom_autoregistration/scheduled_jobs.py
+++ humain_learning/zoom_autoregistration/scheduled_jobs.py
@@ -7,7 +7,7 @@ from frappe.utils import now_datetime, add_to_date
 def get_webinars_for_attendance():
 
 	threshold_time = add_to_date(now_datetime(), minutes=-55)
-	webinars = frappe.get_all("Zoom Webinar", filters={"end_time": ("<=", threshold_time)}, pluck='name')
+	webinars = frappe.get_all("Zoom Webinar", filters={"end_time": ("<=", threshold_time),"attendance_processed":0}, pluck='name')
 
 	for webinar in webinars:
 		frappe.enqueue(
