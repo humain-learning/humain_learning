2672e90ce19cca823d539979ba2be79bc203fb2b
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-06-19 18:34:30 +0530

    Update services.py

--- humain_learning/razorpay_integration/services.py
+++ humain_learning/razorpay_integration/services.py
@@ -3,4 +3,4 @@ import frappe
 def tick_up_coupon(coupon_code):
 	coupon_doc = frappe.get_doc("Coupon Code", coupon_code)
 	coupon_doc.use_count = frappe.utils.cint(coupon_doc.use_count) + 1
-	coupon_doc.save()
+	coupon_doc.save(ignore_permissions=True)
